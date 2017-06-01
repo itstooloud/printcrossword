2e5bd78fbd11b41601b1f0163dedd24d1f8082a2
Chris <chrisstudio@Chriss-iMac.local>
2017-06-01 16:02:18 -0400

    finished date/text function

--- prcw.py
+++ prcw.py
@@ -9,23 +9,31 @@ import datetime
 
 now = datetime.datetime.now()
 
-day = str(now.day)
-month = str(now.month)
-year = str(now.year)
+# day = str(now.day)
+# month = str(now.month)
+# year = str(now.year)
 
-# Function to return the monthname from the month number
+# Functions to return strings used by NYT and LAX in their crossword names
+
 
 def monthname():
 	datenames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 	return datenames[(now.month-1)]
 
 
+def dayname():
+	if len(str(now.day)) == 2:
+		return str(now.day)
+	else:
+		return "0" + str(now.day)
+
+def yearname():
+	return str(now.year)[2:]
 
 
-print('day: ' + day + 'month: ' + month + 'year: ' + year)
+# Not sure yet what the file name rules are but this is a start
 
-print (monthname())
-
+print (monthname() + " " + dayname() + " " + yearname())
 
 #def todaysnytname(date):
 
