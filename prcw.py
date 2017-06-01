6bcd214e25525dc84c60a1ccba51c655f4026bd3
Chris <chrisstudio@Chriss-iMac.local>
2017-06-01 17:36:51 -0400

    creates full URLs for files

--- prcw.py
+++ prcw.py
@@ -9,17 +9,18 @@ import datetime
 
 now = datetime.datetime.now()
 
-# day = str(now.day)
-# month = str(now.month)
-# year = str(now.year)
-
-# Functions to return strings used by NYT and LAX in their crossword names
+#Initialize urls
+nyturl = 'http://www.nytimes.com/svc/crosswords/v2/puzzle/'
+laturl = 'http://www.cruciverb.com/download.php?f='
 
 
 def monthname():
-	datenames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-	return datenames[(now.month-1)]
-
+	#datenames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+	#return datenames[(now.month-1)]
+	if len(str(now.month)) == 2:
+		return str(now.month)
+	else:
+		return "0" + str(now.month)
 
 def dayname():
 	if len(str(now.day)) == 2:
@@ -31,9 +32,26 @@ def yearname():
 	return str(now.year)[2:]
 
 
-# Not sure yet what the file name rules are but this is a start
+def nytpuz():
+	filename = 'daily-' + str(now.year) + '-'
+	filename += monthname() + '-'
+	filename += dayname() + '.puz'
+	return filename
+
+
+def latpuz():
+	return 'lat' + yearname() + monthname() + dayname() + '.puz'
+
+
+
+#Printing out to the screen for debugging purposes
 
 print (monthname() + " " + dayname() + " " + yearname())
+print('Today\'s NYT file: ' + nytpuz())
+print('Today\'s LAX file: ' + latpuz())
+print('NYT URL: ' + nyturl + nytpuz())
+print('LAT URL: ' + laturl + latpuz())
+
 
 #def todaysnytname(date):
 
